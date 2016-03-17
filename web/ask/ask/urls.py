874287f6e74dd9a80298b48da3d4f8d302584af7
tayfen <tayfen@gmail.com>
2016-03-17 18:07:18 +0300

    2.3 full, for dj 1.10

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from qa.views import test
+
 admin.autodiscover()
 
-urlpatterns = patterns("qa.views",
+urlpatterns = [
     # Examples:
-    url(r"^$", "test"),
-    url(r"^login/.*$", "test"),
-    url(r"^signup/.*$", "test"),
-    url(r"^ask/.*$", "test"),
-    url(r"^popular/.*$", "test"),
-    url(r"^new/.*$", "test"),
-    url(r"^question/(\d+)/$", "test"),
+    url(r"^$", test),
+    url(r"^login/.*$", test),
+    url(r"^signup/.*$", test),
+    url(r"^ask/.*$", test),
+    url(r"^popular/.*$", test),
+    url(r"^new/.*$", test),
+    url(r"^question/(\d+)/$", test),
     # url(r"^blog/", include("blog.urls")),
     # url(r"^admin/", include(admin.site.urls)),
-)
+]
 
